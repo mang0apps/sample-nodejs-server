0a8e0d287ef3f6917fa3817e94d67ccb97a86f50
richmond <richmondcoca@gmail.com>
2025-09-18 12:55:05 +0800

    Add get-url route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import express
 const express = require('express');
+const { default: Innertube, UniversalCache } = require('youtubei.js');
 const app = express();
 const port = 3000;
 
@@ -7,20 +8,19 @@ const port = 3000;
 app.use(express.json());
 
 // Root route
-app.get('/', (req, res) => {
-  res.send('Hello World from Express.js!');
-});
-
-// Example GET with params
-app.get('/hello/:name', (req, res) => {
-  res.send(`Hello, ${req.params.name}!`);
-});
-
-// Example POST
-app.post('/echo', (req, res) => {
-  res.json({
-    received: req.body,
-  });
+app.get('/get-url/:id', async (req, res) => {
+  try {
+    const yt = await Innertube.create({ cache: new UniversalCache(false), generate_session_locally: true });
+    const info = await yt.getStreamingData(req.params.id, {
+      // client: "YTMUSIC",
+      // itag: 18
+      type: "video+audio",
+      format: "mp4"
+    })
+    res.json({url: info.url}); 
+  } catch (error) {
+    res.json({error})
+  }
 });
 
 // Start server
