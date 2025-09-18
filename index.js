7001b63492985781c627b82a407517d0d92ee2df
richmond <richmondcoca@gmail.com>
2025-09-18 13:21:24 +0800

    Add @distube/ytdl-core

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import express
+const ytdl = require('@distube/ytdl-core');
 const express = require('express');
-const { default: Innertube, UniversalCache } = require('youtubei.js');
+// const { default: Innertube, UniversalCache } = require('youtubei.js');
 const app = express();
 const port = 3000;
 
@@ -10,14 +11,16 @@ app.use(express.json());
 // Root route
 app.get('/get-url/:id', async (req, res) => {
   try {
-    const yt = await Innertube.create({ cache: new UniversalCache(false), generate_session_locally: true });
-    const info = await yt.getStreamingData(req.params.id, {
-      // client: "YTMUSIC",
-      // itag: 18
-      type: "video+audio",
-      format: "mp4"
-    })
-    res.json({url: info.url}); 
+    // const yt = await Innertube.create({ cache: new UniversalCache(false), generate_session_locally: true });
+    // const info = await yt.getStreamingData(req.params.id, {
+    //   // client: "YTMUSIC",
+    //   // itag: 18
+    //   type: "video+audio",
+    //   format: "mp4"
+    // })
+    // res.json({url: info.url}); 
+    const info = await ytdl.getInfo(req.params.id);
+    return res.json(info.formats.filter(item => item.hasAudio && item.hasVideo)?.[0]?.url ?? '');
   } catch (error) {
     res.json({error})
   }
